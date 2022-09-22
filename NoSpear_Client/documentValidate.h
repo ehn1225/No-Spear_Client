01c823ef55af7fac6f19fd3bafc9efb3f6e8c951
LeeYeChan <ehn1225@naver.com>
2022-09-22 16:58:18 +0900

    [No-Spear][이예찬] 문서 파일 유효서 검사 추가, 에러 처리 추가

---
+++ NoSpear_Client/documentValidate.h
@@ -0,0 +1,16 @@
+#ifndef DOCUMENTVALIDATE_H
+#define DOCUMENTVALIDATE_H
+
+class DocumentValidate
+{
+public:
+    DocumentValidate();
+    bool readSignature(std::string filePath); // 반환값 false는 문서파일이 아닌경우 true는 문서파일인 경우 / 인자값 해당 파일의 절대경로 지정
+
+private:
+    std::vector<std::vector<int>> headerFileSignatureHexV;
+    std::vector<int> headerFileSignatureHex;
+    std::vector<std::string> fileSignature;
+};
+
+#endif // DOCUMENTVALIDATE_H
